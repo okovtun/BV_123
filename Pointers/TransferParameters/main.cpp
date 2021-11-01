d41d5fd6e50e0bf9526c343187f02f8cc62f6cc4
Clayman <Clayman@DESKTOP-DM8E77U>
2021-11-01 18:59:07 +0200

    Parameters by value

--- Pointers/TransferParameters/main.cpp
+++ Pointers/TransferParameters/main.cpp
@@ -1,7 +1,14 @@
 #include<iostream>
 using namespace std;
 
-void Exchange(int a, int b);
+/*
+1. По значению(by value);
+2. По указателю(by pointer);
+3. По ссылке(by reference);
+*/
+
+//Прототип (Объявление) функции - Function declaration:
+void Exchange(int a, int b/*Принимаемые параметры*/);
 
 void main()
 {
@@ -9,14 +16,13 @@ void main()
 	int a = 2;
 	int b = 3;
 	cout << a << "\t" << b << endl;
-	cout << &a << "\t" << &b << endl;
-	Exchange(a, b);
+	Exchange(a, b/*передаем параметры в функцию*/);//Вызов функции (Function call)
 	cout << a << "\t" << b << endl;
 }
 
-void Exchange(int a, int b)
+//Реализация (определение) функции - Function definition:
+void Exchange(int a, int b/*Принимаемые параметры*/)
 {
-	cout << &a << "\t" << &b << endl;
 	int buffer = a;
 	a = b;
 	b = buffer;
